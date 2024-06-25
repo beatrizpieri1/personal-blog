4dd75fa45e4bcfec5ae36e04ca0219407a83956a
beatrizpieri1 <161755243+beatrizpieri1@users.noreply.github.com>
2024-06-25 18:15:26 -0300

    navbar apenas topicos

--- src/javaScript.js
+++ src/javaScript.js
@@ -1,8 +1,20 @@
-const menuBtn = document.getElementById("menu-btn")
-const menuBar = document.getElementById("menu-bar")
-const closeBtn = document.getElementById("close-btn")
+const menuBtn = document.getElementById("menu-btn");
+const menuBar = document.getElementById("menu-bar");
+const closeBtn = document.getElementById("close-btn");
+const bodyDiv = document.getElementById("body")
 
 
-menuBtn.addEventListener("click", function(){
-    menuBar.style.display = "flex";
+menuBtn.addEventListener("click", function () {
+  menuBar.classList.remove("hidden");
 });
+
+
+closeBtn.addEventListener("click", function () {
+  menuBar.classList.add("hidden");
+});
+
+window.addEventListener("click", function(event) {
+    if (event.target === bodyDiv) {
+      menuBar.classList.add("hidden");
+    }
+  });
